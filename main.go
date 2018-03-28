87fac41df048117d701b48c6bee12334002cf70d
Jackson Sabey <jackson@juandefu.ca>
2018-03-27 20:17:56 -0700

    added services to config.json, also made a note of why the marshalled output is the way it is

--- main.go
+++ main.go
@@ -20,6 +20,17 @@ func main() {
 		log.Fatalf("failed to create patrol: %s\n", err)
 		return
 	}
+	// when printing a services ignored exit codes, []uint8 will be outputted as a string and it's value is base64 encoded
+	// the reason for this is that go may either be assuming this is a string or that it's more efficient to use base64 than to output a bunch of integers
+	// except, if we were to use the type int8 or even any other type of int the output would not use base64 as a representation
+	// if we were to use the outputted base64 value in our config.json, encoding/json would decode the base64 without problem
+	//
+	// unmarshalling:
+	//	"ignore-exit-codes": "/w=="
+	// is the same as:
+	//	"ignore-exit-codes": [
+	//		255
+	//	]
 	bs, _ := json.MarshalIndent(config, "", "\t")
 	fmt.Printf("config %s\n", bs)
 }
