1b5a67c19abea2c1535a46b3278fae29d5de9c40
Jackson Sabey <jackson@juandefu.ca>
2018-03-30 00:29:03 -0700

    added a testapp that will log PID as soon as it starts and then it waits to be signalled to stop

---
+++ unittest/testapp/main.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+func main() {
+	start := time.Now()
+	// log and fmt will be spread out throughout this app
+	// the reason for this is to send half of the msgs to stdout and the other to stderr
+	log.Println("hello, I am a test app. I will run until you signal me to stop!")
+	fmt.Printf("the time is now: %s\n", start)
+	// open testapp.pid
+	// truncate and open file, create file if it doesn't exist
+	file, err := os.OpenFile("testapp.pid", os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
+	if err != nil {
+		log.Fatalf("failed to open testapp.pid: \"%s\"\n", err)
+		return
+	}
+	// write our PID to testapp.pid
+	// append newline as well, clients should be prepared to handle whitespace themselves
+	if _, err := file.WriteString(fmt.Sprintf("%d\n", os.Getpid())); err != nil {
+		file.Close()
+		log.Fatalf("failed to write to file: \"%s\"\n", err)
+		return
+	}
+	if err := file.Close(); err != nil {
+		log.Fatalf("failed to close file: \"%s\"\n", err)
+		return
+	}
+	fmt.Printf("I've written our PID: %d to testapp.pid\n", os.Getpid())
+	// create an unbuffered channel to listen for signals
+	signals := make(chan os.Signal)
+	// send signal notifications to our variable `signals`
+	signal.Notify(
+		signals,
+		// kill -1 PID
+		syscall.SIGHUP,
+		// kill -2 PID
+		// this is the same as ctrl+c from the CLI
+		syscall.SIGINT,
+		// we're NOT going to quit on these! we're just going to log that we received them
+		// kill -10 PID
+		syscall.SIGUSR1,
+		// kill -12 PID
+		syscall.SIGUSR2,
+	)
+	fmt.Println("we're going to block and wait for a signal")
+	for {
+		done := false
+		select {
+		case <-time.After(time.Second * 30):
+			// we haven't received any signals, lets let people know
+			log.Println("still waiting for you to signal me!")
+		case sig := <-signals:
+			// we've received a signal, let's figure out what one it was!
+			// we can use a switch statement to determine our signal type
+			switch sig {
+			case syscall.SIGHUP:
+				log.Println("Received SIGHUP, closing!")
+				done = true
+				break
+			case syscall.SIGINT:
+				// terminate process
+				// ctrl+c
+				log.Println("Received SIGINT, closing!")
+				done = true
+				break
+			case syscall.SIGUSR1:
+				fmt.Println("Received SIGUSR1, ignoring!")
+			case syscall.SIGUSR2:
+				log.Println("Received SIGUSR2, ignoring!")
+			default:
+				// this shouldn't happen
+				log.Fatalf("Received Unknown?: %v\n", sig)
+			}
+		}
+		if done {
+			// exit loop and quit
+			break
+		}
+	}
+	fmt.Printf("we ran for %s\n", time.Now().Sub(start))
+	log.Println("good bye!")
+}
