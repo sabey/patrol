1fef15a80a3e7bcf36f283b4e26cdd60a11308e6
Jackson Sabey <jackson@juandefu.ca>
2018-03-30 02:54:24 -0700

    added keepalive flag for our different keepalive methods - we default to 2, writing our PID

--- unittest/testapp/main.go
+++ unittest/testapp/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"os"
@@ -9,31 +10,54 @@ import (
 	"time"
 )
 
+var (
+	keepalive = flag.Int("keepalive", 2, "KeepAlive Method: (1: \"Patrol\", 2: \"App\", 3: \"HTTP\", 4: \"UDP\")")
+)
+
 func main() {
 	start := time.Now()
+	if !flag.Parsed() {
+		flag.Parse()
+	}
 	// log and fmt will be spread out throughout this app
 	// the reason for this is to send half of the msgs to stdout and the other to stderr
 	log.Println("hello, I am a test app. I will run until you signal me to stop!")
 	fmt.Printf("the time is now: %s\n", start)
-	// open testapp.pid
-	// truncate and open file, create file if it doesn't exist
-	file, err := os.OpenFile("testapp.pid", os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
-	if err != nil {
-		log.Fatalf("failed to open testapp.pid: \"%s\"\n", err)
+	go func() {
+		<-time.After(time.Minute * 5)
+		log.Fatalln("testapp ran for too long, killing process")
+	}()
+	if *keepalive == 1 {
+		log.Printf("KeepAlive: APP_KEEPALIVE_PID_PATROL - we will NOT write PID: %d to file!", os.Getpid())
+	} else if *keepalive == 2 {
+		log.Println("KeepAlive: APP_KEEPALIVE_PID_APP")
+		// open testapp.pid
+		// truncate and open file, create file if it doesn't exist
+		file, err := os.OpenFile("testapp.pid", os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
+		if err != nil {
+			log.Fatalf("failed to open testapp.pid: \"%s\"\n", err)
+			return
+		}
+		// write our PID to testapp.pid
+		// append newline as well, clients should be prepared to handle whitespace themselves
+		if _, err := file.WriteString(fmt.Sprintf("%d\n", os.Getpid())); err != nil {
+			file.Close()
+			log.Fatalf("failed to write to file: \"%s\"\n", err)
+			return
+		}
+		if err := file.Close(); err != nil {
+			log.Fatalf("failed to close file: \"%s\"\n", err)
+			return
+		}
+		fmt.Printf("I've written our PID: %d to testapp.pid\n", os.Getpid())
+	} else if *keepalive == 3 {
+		log.Fatalln("KeepAlive: APP_KEEPALIVE_HTTP - NOT IMPLEMENTED")
+	} else if *keepalive == 4 {
+		log.Fatalln("KeepAlive: APP_KEEPALIVE_UDP - NOT IMPLEMENTED")
+	} else {
+		log.Fatalln("Unknown KeepAlive Method passed as a flag!")
 		return
 	}
-	// write our PID to testapp.pid
-	// append newline as well, clients should be prepared to handle whitespace themselves
-	if _, err := file.WriteString(fmt.Sprintf("%d\n", os.Getpid())); err != nil {
-		file.Close()
-		log.Fatalf("failed to write to file: \"%s\"\n", err)
-		return
-	}
-	if err := file.Close(); err != nil {
-		log.Fatalf("failed to close file: \"%s\"\n", err)
-		return
-	}
-	fmt.Printf("I've written our PID: %d to testapp.pid\n", os.Getpid())
 	// create an unbuffered channel to listen for signals
 	signals := make(chan os.Signal)
 	// send signal notifications to our variable `signals`
@@ -54,7 +78,7 @@ func main() {
 	for {
 		done := false
 		select {
-		case <-time.After(time.Second * 30):
+		case <-time.After(time.Second * 10):
 			// we haven't received any signals, lets let people know
 			log.Println("still waiting for you to signal me!")
 		case sig := <-signals:
